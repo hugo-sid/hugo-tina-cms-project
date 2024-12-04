82c79c92474e5b1df1d35d67ec9013e2e82aaeb1
Sid <122173059+hugo-sid@users.noreply.github.com>
2024-12-04 22:48:05 +0000

    fix: upgrade packages, remove secrets

--- tina/config.js
+++ tina/config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from "tinacms";
 
 // Your hosting provider likely exposes this as an environment variable
-const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "master";
 
 export default defineConfig({
   branch,
-  clientId: "126c7fbe-d7ed-49ea-8841-a5b535fd8595", // Get this from tina.io
-  token: "5647dec5e6d06b3c23c245d869982cbf5bf59577", // Get this from tina.io
+  clientId: " ", // Get this from tina.io
+  token: " ", // Get this from tina.io
+
 
   build: {
     outputFolder: "admin",
